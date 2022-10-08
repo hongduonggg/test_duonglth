9f3acda328902d874f43b23e94a3e3c973ab3405
Nguyen Tan Viet <nguyentanviet@cryptopie-labo.com>
2022-10-08 15:29:15 +0700

    init prj

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        vcc();
+    }
+
+    static void vcc() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Duong ngu");
+        }
+    }
+}
